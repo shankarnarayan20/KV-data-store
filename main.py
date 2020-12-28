50c66b8813457331c2cf3ba0ee17bdf358e93892
Shankar Narayan <shankarnarayan20@gmail.com>
2020-12-28 19:14:45 +0530

    added time to live and created feature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import flask
 import json
 from filepath import filepath
+from datetime import datetime
 
 path = filepath()
 app = flask.Flask(__name__)
@@ -11,17 +12,23 @@ app.config["DEBUG"] = True
 def home():
     return "Key Value Data Store by Shankar Narayan",200
 
-@app.route('/create/', methods=['POST'])
+@app.route('/create', methods=['POST'])
 def create():
     req_data = flask.request.get_json()
-
-    for allkeys in req_data:
-        if len(allkeys)>32:
+    now = datetime.now()
+    nowasstring = now.strftime("%d/%m/%Y %H:%M:%S")
+    
+    for req_datakeys in req_data:
+        if len(req_datakeys)>32:
             return "length error",400
-
-    for key in req_data:
-        print(req_data[key].__sizeof__())
-
+        req_data[req_datakeys]['CreatedAt'] = nowasstring
+        if 'timetolive' in req_data[req_datakeys].keys():
+            try:
+                int(req_data[req_datakeys]['timetolive'])
+                pass
+            except:
+                return "timetolive is not an integer",400    
+    
     with open('./data/data.json') as f:
         data = json.load(f)
     
